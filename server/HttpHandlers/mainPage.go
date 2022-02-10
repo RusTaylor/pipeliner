c639eae2be9ff9f8cb7104436ba25ee319cbc182
RusTaylor <rustaylor.web@gmail.com>
2022-02-11 00:54:19 +0300

    refactor Index method

--- server/HttpHandlers/mainPage.go
+++ server/HttpHandlers/mainPage.go
@@ -9,15 +9,23 @@ import (
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	if !session.IsLogin(r) {
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		w.Header().Set("Cache-Control", "no-cache, private") // HTTP 1.1.
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
 	}
 
 	tmpl, err := template.ParseFiles("public/index.html")
 	if err != nil {
 		log.Println(err)
 	}
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		log.Println(err)
+	}
 
-	err = tmpl.Execute(w, nil)
+	user := session.GetUser(cookie.Value)
+
+	err = tmpl.Execute(w, user)
 
 	if err != nil {
 		log.Println(err)
