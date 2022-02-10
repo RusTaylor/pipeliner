2ff05a79607bf764982574160d91d4376d17e7a7
RusTaylor <rustaylor.web@gmail.com>
2022-02-11 00:54:43 +0300

    refactor login and auth routes and add favicon route

--- server/route.go
+++ server/route.go
@@ -6,10 +6,14 @@ import (
 )
 
 func setRoutes() {
-	http.HandleFunc("/login/", HttpHandlers.LoginPage)
-	http.HandleFunc("/auth/", HttpHandlers.Auth)
+	http.HandleFunc("/login", HttpHandlers.LoginPage)
+	http.HandleFunc("/auth", HttpHandlers.Auth)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
-
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	//Роуты требующие проверки на логин
 	http.HandleFunc("/", HttpHandlers.Index)
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/favicon.ico")
 }
