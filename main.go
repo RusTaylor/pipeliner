c368a31ada0ef5dee57e5dd84692f295f1ebb064
RusTaylor <rustaylor.web@gmail.com>
2022-02-16 00:07:58 +0300

    call SetConfig and Migrate method to main func

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"pipeliner/config"
+	"pipeliner/migration"
 	"pipeliner/server"
 )
 
 func main() {
+	config.SetConfig()
+	migration.Migrate()
 	//todo проверка на наличие баз данных и если нет, то создать
-	server.StartHTTPServer("pipeliner", "8080")
+	server.StartHTTPServer(config.Config.Server.Host, config.Config.Server.Port)
 }
